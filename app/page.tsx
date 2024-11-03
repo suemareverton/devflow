8854a1734ca0e9754634f76517c6932843b0b07c
Suemar Éverton <suemareverton@gmail.com>
2024-11-02 22:10:07 -0300

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-white font-black">Welcome to the world of Next.js</h1>
+      <h1 className="text-3xl font-black text-white">
+        Welcome to the world of Next.js
+      </h1>
     </div>
   );
-}
+};
 
 export default Home;
