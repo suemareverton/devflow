f2fb9196e7abc356f9b75f0b39e5188b4b485f30
Suemar Éverton <suemareverton@gmail.com>
2024-11-02 23:08:40 -0300

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 const Home = () => {
   return (
-    <div>
+    <>
       <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-    </div>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the world of Next.js
+      </h1>
+    </>
   );
 };
 
