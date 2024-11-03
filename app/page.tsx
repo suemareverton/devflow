0f150d999aa23ed2dc03f0b2e3d84cae4744bc8b
Suemar Éverton <suemareverton@gmail.com>
2024-11-02 23:54:28 -0300

    Shadcn UI setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const Home = () => {
   return (
     <>
@@ -5,6 +14,17 @@ const Home = () => {
       <h1 className="h1-bold font-space-grotesk">
         Welcome to the world of Next.js
       </h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
