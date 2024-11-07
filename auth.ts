9b4a6a6a5f9c90f23e8101ef8a4b5abf0152a493
Suemar Éverton <suemareverton@gmail.com>
2024-11-07 15:55:41 -0300

    Implement Google SignIn

--- auth.ts
+++ auth.ts
@@ -9,7 +9,8 @@
 
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
